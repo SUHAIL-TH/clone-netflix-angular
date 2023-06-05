1a80db81fe8e389504dae5157373a49367f56baa
Suhail TH <suhailth17756@gmail.com>
2023-06-06 01:11:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,17 @@ import { Component ,HostListener} from '@angular/core';
 })
 export class AppComponent {
   title = 'showtime';
+  navbg:any;
   @HostListener('document:scroll') scrollover() {
     console.log(document.body.scrollTop, 'scrolllength#');
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-  
+      this.navbg={
+        'background-color':'#000000'
+      }
     } else {
-  
+      this.navbg={
+        
+      }
     }
   };
 }

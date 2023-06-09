5ce25b0244582b03181325dc2176dc673aac96de
Suhail TH <suhailth17756@gmail.com>
2023-06-09 10:42:04 +0530

    add service

--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -20,9 +20,56 @@ export class MovieApiServiceService {
     return this.http.get(`${this.baseurl}/trending/movie/day?api_key=${this.apikey}`)
   }
   // searchingMovies
+
   getSearchMovie(data:any):Observable<any>{
     console.log(data,'movies#' );
     
     return this.http.get(`${this.baseurl}/search/movie?api_key=${this.apikey}&query=${data.movieName}`)
   }
+
+  //getmoviedetails
+  getMovieDetails(data:any):Observable<any>{
+    return this.http.get(`${this.baseurl}/movie/${data}?api_key=${this.apikey}`)
+  }
+//getmoviedata
+  getMovieVedeo(data:any):Observable<any>{
+    return this.http.get(`${this.baseurl}/movie/${data}/videos?api_key=${this.apikey}`)
+  }
+  //getmoviecast
+  getMovieCast(data:any):Observable<any>{
+    return this.http.get(`${this.baseurl}/movie/${data}/credits?api_key=${this.apikey}`)
+  }
+  fetchActionMovies(): Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=28`);
+  }
+  
+  // adventure
+  fetchAdventureMovies(): Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=12`);
+  }
+  
+  // animation
+  fetchAnimationMovies(): Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=16`);
+  }
+  // comedy
+fetchComedyMovies(): Observable<any> {
+  return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=35`);
+}
+
+// documentary
+fetchDocumentaryMovies(): Observable<any> {
+  return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=99`);
+}
+
+// science-fiction:878
+
+fetchScienceFictionMovies(): Observable<any> {
+  return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=878`);
+}
+
+// thriller:53
+fetchThrillerMovies(): Observable<any> {
+  return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=53`);
+}
 }
